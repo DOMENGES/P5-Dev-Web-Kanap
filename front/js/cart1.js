68e32b7a71b4927d5fdef56a9816c534aa87ebca
Marielle Domenges <marielle.domenges@gmail.com>
2023-09-06 21:15:26 +0200

    total price et quantité terminés

---
+++ front/js/cart1.js
@@ -0,0 +1,78 @@
+// récupération du prix dans l'API
+ async function fetchProductPrice(productId) {
+  let urlItem = "http://localhost:3000/api/products/" + productId;
+  console.log(urlItem);
+  let response = await fetch(urlItem);
+  let data = await response.json();
+  let productPrice = data.price;
+  console.log(productPrice);
+  return productPrice;
+}
+// calcul et affichage totalQuantity
+function totalQuantity_Price (){
+  let cart__items = window.localStorage.getItem("items");
+  let itemsJson = JSON.parse(cart__items);
+  let totalQuantity = 0;
+  let totalPrice = 0;
+  itemsJson.map(result =>{
+    totalQuantity += result.quantity;
+    totalPrice += result.quantity*result.price;
+
+  document.querySelector (`#totalQuantity`).innerHTML = totalQuantity;
+  document.querySelector(`#totalPrice`).innerHTML = totalPrice;
+})
+}
+
+// changer le totalQuantity
+function changeQuantity(){
+  const btnAddToCart = parseInt(document.querySelector("input .itemQuantity"));
+  // let qte = parseInt(btnAddToCart);
+  console.log(btnAddToCart);
+  // btnAddToCart.addEventListener("change",(event)=> {
+  //     console.log(event.value);
+  //   })
+}
+// récupération tous les items du localStorage
+  async function afficherCart__item (){
+  let cart__items = window.localStorage.getItem("items");
+  let itemsJson = JSON.parse(cart__items);
+  for (let i=0; i<itemsJson.length; i++){
+    let cart__item = itemsJson[i];
+    let productId = cart__item.id;
+    let productColor = cart__item.color;
+    let productUrl = cart__item.imageUrl;
+    let productName = cart__item.name;
+    let productQuantity = cart__item.quantity;
+    let productPrice = await fetchProductPrice(productId);
+    
+    cart__items = document.getElementById("cart__items");
+    cart__item = 
+    `<article class="cart__item">  ${productId} ${productColor}
+    <div class="cart__item__img">
+      <img src="${productUrl}" alt="Photographie d'un canapé">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${productName}</h2>
+        <p>${productColor}</p>
+        <p>${productPrice} €</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : ${productQuantity}</p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article> `
+  cart__items.innerHTML += cart__item;
+  }
+}
+afficherCart__item();
+totalQuantity_Price();
+changeQuantity();
+// fetchProductPrice()
+// 
