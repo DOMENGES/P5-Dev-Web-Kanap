d44e9971a53e10af480917ddc840e2e3fa3fc78b
Marielle Domenges <marielle.domenges@gmail.com>
2023-09-28 23:22:14 +0200

    presqueFin RegExp

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,110 +1,203 @@
-// récupération du prix dans l'API puis traduction en format Json
-// calcul du prix total avec quantité du localStorage
-// dépend de la complétude de displayCartElements pour récupérer
-// l'argument productId
-// la fonction retourne une promesse
-async function fetchProductPrice(productId, qte){
-  // attendre d'avoir attrapé l'API d'un produit avec son id
-  const response = await fetch(`http://localhost:3000/api/products/${productId}`);
-  // attendre la traduction du data (récupéré dans l'API) en format json
-  const data = await response.json();
-  // pour retourner une promesse
-  let total = qte * data.price;
-  return total;
-}//fin fetchProductPrice
 
-function getTotalPrice() {
-  // récupération de tous les produits dans le localStorage et traduction en format JSON
+async function fetchProductPrice(productId) {
+  let urlItem = "http://localhost:3000/api/products/" + productId;
+  // console.log(urlItem);
+  let response = await fetch(urlItem);
+  let data = await response.json();
+  let productPrice = data.price;
+  return productPrice;
+}
+// calcul et affichage totalQuantity
+async function totalFinalPrice() {
   let cart__items = window.localStorage.getItem("items");
   let itemsJson = JSON.parse(cart__items);
-  let total = 0;
-  let qte = 0;
-  // récupération des valeurs des propriétés price et quantity de chaque produit
-  // du tableau itemsJson
-  // calcul du prix total (price*quantity) de chaque produit
-  itemsJson.map(
-    res => {
-      total += res.price * res.quantity;
-      qte += res.quantity;
-    }
-  )
-  // affichage dans la page Web du prix total et de la quantité totale
-  document.querySelector (`#totalPrice`).innerHTML = total;
-  document.querySelector (`#totalQuantity`).innerHTML = qte;
-} //fin getTotalPrice
+  let totalQuantity = 0;
+  let totalPrice = 0;
 
-// récupération des valeurs propriétés des produits du localStorage
-// excepté le prix des produits
-// la fonction retourne une promesse
-async function displayCartElements() {
+  for (const result of itemsJson) {
+      let productPrice = await fetchProductPrice(result.id);
+      // productQuantity = result.quantity;
+      // console.log(productQuantity);
+      totalPrice += result.quantity * productPrice;
+      totalQuantity += result.quantity;
+
+      // document.querySelector ()
+      document.querySelector (`#totalQuantity`).innerHTML = totalQuantity;
+      document.querySelector(`#totalPrice`).innerHTML = totalPrice;
+  }
+}
+// 
+// récupérer la valeur de input class itemQuantity
+// changer result.quantity par input class itemQuantity
+
+// function mafonction(){
+//   console.log(click)
+// }
+// addEventListener("click", mafonction);
+// var quantityInputs = document.getElementsByClassName("itemQuantity");
+// console.log(quantityInputs);
+
+
+
+
+
+// récupération tous les items du localStorage
+async function afficherCart__item (){
   let cart__items = window.localStorage.getItem("items");
   let itemsJson = JSON.parse(cart__items);
   for (let i=0; i<itemsJson.length; i++){
-    let cart__item = itemsJson[i];
-    let productId = cart__item.id;
-    let productColor = cart__item.color;
-    let productUrl = cart__item.imageUrl;
-    let productName = cart__item.name;
-    let productQuantity = cart__item.quantity;
-    // attendre que la fonction fetchProductPrice calcule le total produit
-    // calcul du prix total avec quantité du localStorage
-    let productPrice = await fetchProductPrice(productId, productQuantity);
+      let cart__item = itemsJson[i];
+      let productId = cart__item.id;
+      let productColor = cart__item.color;
+      let productUrl = cart__item.imageUrl;
+      let productName = cart__item.name;
+      let productQuantity = cart__item.quantity;
+      let productPrice = await fetchProductPrice(productId);
 
-    cart__items = document.getElementById("cart__items");
-// affichage d'UNE carte d'UN produit
-    cart__item = `
-              <article class="cart__item"> ${productId}  ${ productColor}
-                <div class="cart__item__img">
-                  <img src="${productUrl}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>  ${productName}</h2>
-                    <p>  ${productColor}</p>
-                    <p> ${productPrice} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${productQuantity} </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
-                    </div>
-                  <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem">Supprimer</p>
-                  </div>
-                </div>
-              </article>`;
-// affichage de toute les cartes produits
-              cart__items.innerHTML += cart__item;          
-  }// fin parcourt de la boucle dans le tableau itemsJson
-}// fin displayCartElements
-// appel fonction displayCartElements pour affichage de toutes les cartes
-displayCartElements();
-// appel fonction getTotalPrice
-getTotalPrice();
-
-// vérification et test firstName et Name
-function validerPrenomNom(firstName,lastName){
-  let nom = firstName;
-  nom = lastName;
-  if(nom.length>=2){
-    return true;
+      cart__items = document.getElementById("cart__items");
+      cart__item =
+          `<article class="cart__item">
+  <div class="cart__item__img">
+    <img src="${productUrl}" alt="Photographie d'un canapé">
+  </div>
+  <div class="cart__item__content">
+    <div class="cart__item__content__description">
+      <h2>${productName}</h2>
+      <p>${productColor}</p>
+      <p>${productPrice} €</p>
+    </div>
+    <div class="cart__item__content__settings">
+      <div class="cart__item__content__settings__quantity">
+      <p>Qté : ${productQuantity} </p>
+      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+      </div>
+      <div class="cart__item__content__settings__delete">
+        <p class="deleteItem">Supprimer</p>
+      </div>
+    </div>
+  </div>
+</article> `
+      cart__items.innerHTML += cart__item;
   }
-  return false;
 }
-// évènement click de "commander !"
-const form = document.getElementById("order");
-form.addEventListener("click", (Event) =>{
-  Event.preventDefault();
-// formulaire firstName
-let baliseFirstName = document.getElementById("firstName");
-let firstName = baliseFirstName.value;
-console.log(firstName);
-// formulaire lastName
-let baliseLastName = document.getElementById("lastName");
-let lastName = baliseLastName.value;
-// validation firstName et lastName
-if(validerPrenomNom(firstName) && validerPrenomNom(lastName)){
-  return console.log(firstName, lastName);
-}else
-{ console.log("erreur")}
+afficherCart__item();
+
+totalFinalPrice();
+
+
+// test champs
+// function verifierChamp(balise){
+// let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+// firstNameErrorMsg = alert("ceci n'est pas un prénom");
+// let PrenomRegExp = new RegExp("^[A-zÀ-ú]+$");
+// if (PrenomRegExp.test(balise.value)){
+//   balise.classList.remove("error");
+// } else {
+//   balise.classList.add("error");
+  // balise.addEventListener("change",()=>{
+  //   verifierPrenom(balise)
+  // })
+// }
+// }
+// function recommencerBalise(balise){
+//   balise.addEventListener("change",()=>{
+
+//   })
+// }
+
+// tests formulaire cart__order__form
+// let form = document.querySelector("form");
+// let balisePrenom = document.getElementById("firstName");
+// let baliseNom = document.getElementById("lastName");
+// let baliseAdresse = document.getElementById("address");
+// let baliseVille = document.getElementById("city");
+// let baliseEmail = document.getElementById("email");
+
+// form.addEventListener("submit", (event)=>{
+//   event.preventDefault();
+// verifierChamp(balisePrenom);
+//   let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+//   firstNameErrorMsg = alert("ceci n'est pas un prénom");
+//   let PrenomRegExp = new RegExp("^[A-zÀ-ú]+$");
+//   if (PrenomRegExp.test(balisePrenom.value)){
+//     balisePrenom.classList.remove(firstNameErrorMsg);
+//   } else {
+//     balisePrenom.classList.add(firstNameErrorMsg);
+//   }
+//   console.log(balisePrenom.value);
+//   console.log(baliseNom.value);
+//   console.log(baliseAdresse.value);
+//   console.log(baliseVille.value);
+//   console.log(baliseEmail.value)
+// })
+
+// 
+let balisePrenom = document.getElementById("firstName");
+let baliseNom = document.getElementById("lastName");
+let baliseAdresse = document.getElementById("address");
+let baliseVille = document.getElementById("city");
+let baliseEmail = document.getElementById("email");
+balisePrenom.addEventListener("change", ()=>{
+let prenomRegExp = new RegExp("^[A-zÀ-ú- +\.]+$");
+if (prenomRegExp.test(balisePrenom.value)){
+  document.getElementById(`firstName`).innerHTML = "balisePrenom.value";
+  document.getElementById(`firstNameErrorMsg`).innerHTML = "";
+  // let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+  // firstNameErrorMsg = "ceci n'est pas un prénom";
+  // balisePrenom.classList.remove(firstNameErrorMsg);
+} else {
+  // let firstNameErrorMsg = document.getElementById("firstNameErrorMsg");
+  // console.log(firstNameErrorMsg);
+  document.getElementById(`firstNameErrorMsg`).innerHTML = "ceci n'est pas un prénom";
+  // firstNameErrorMsg = "ceci n'est pas un prénom";
+  // balisePrenom.classList.add(firstNameErrorMsg);
+}
 })
+baliseNom.addEventListener("change", ()=>{
+let nomRegExp = new RegExp("^[A-zÀ-ú- +\.]+$");
+if (nomRegExp.test(baliseNom.value)){
+document.getElementById(`lastName`).innerHTML = "baliseNom.value";
+document.getElementById(`lastNameErrorMsg`).innerHTML = "";
+} else {
+  document.getElementById(`lastNameErrorMsg`).innerHTML = "ceci n'est pas un nom";
+}
+})
+baliseAdresse.addEventListener("change", ()=>{
+let adresseRegExp = new RegExp("^[A-zÀ-ú- +\.0-9^,\/]+$");
+if (adresseRegExp.test(baliseAdresse.value)){
+document.getElementById(`address`).innerHTML = "baliseVille.value";
+document.getElementById(`addressErrorMsg`).innerHTML = "";
+} else {
+  document.getElementById(`addressErrorMsg`).innerHTML = "ceci n'est pas une adresse";
+}
+})
+baliseVille.addEventListener("change", ()=>{
+let villeRegExp = new RegExp("^[A-zÀ-ú- 0-9^,\/]+$");
+if (villeRegExp.test(baliseVille.value)){
+document.getElementById(`city`).innerHTML = "baliseVille.value";
+document.getElementById(`cityErrorMsg`).innerHTML = "";
+} else {
+  document.getElementById(`cityErrorMsg`).innerHTML = "ceci n'est pas une ville";
+}
+})
+baliseEmail.addEventListener("change", ()=>{
+
+let emailRegExp = new RegExp("^[a-z.@-]+$");
+if (emailRegExp.test(baliseEmail.value)){
+document.getElementById(`email`).innerHTML = "baliseEmail.value";
+document.getElementById(`emailErrorMsg`).innerHTML = "";
+} else {
+  document.getElementById(`emailErrorMsg`).innerHTML = "ceci n'est pas une adresse mail";
+}
+})
+// class contactClient {
+// constructor(prenom,nom, adresse, ville, email){
+//   this.prenom = prenom;
+//   this.nom = nom;
+//   this.adresse = adresse;
+//   this.ville = ville;
+//   this.email = email;
+// }
+// }
+// let client1 = new contactClient("balisePrenom.value","baliseNom.value","baliseAdresse.value","baliseVille.value","baliseEmail.value");
+// console.log(client1);
